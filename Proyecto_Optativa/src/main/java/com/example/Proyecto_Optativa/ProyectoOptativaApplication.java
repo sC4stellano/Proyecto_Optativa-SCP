f1b6e539abe297f2fbb5deee0041681374a4b68b
sC4stellano <sergiojose.castellano@a.vedrunasevillasj.es>
2025-11-18 19:28:53 +0100

    Primer commit

---
+++ Proyecto_Optativa/src/main/java/com/example/Proyecto_Optativa/ProyectoOptativaApplication.java
@@ -0,0 +1,13 @@
+package com.example.Proyecto_Optativa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoOptativaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoOptativaApplication.class, args);
+	}
+
+}
---
+++ Proyecto_Optativa/src/test/java/com/example/Proyecto_Optativa/ProyectoOptativaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Proyecto_Optativa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoOptativaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
